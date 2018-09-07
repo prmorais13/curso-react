3cd552782cf878a8a7f35848b5b684f37ece3bc1
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-07 00:14:23 -0300

    39. Testando a API (Postman) e 40. Habilitando o CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const AllowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(AllowCors)
 
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${ port }`)
