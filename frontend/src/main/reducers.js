c135a7d3a367af8a24a4088d5cf0deb6bf9efc67
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-07 23:39:35 -0300

    71. Instalando as Dependências e 72. Configurando os Reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,26 @@
+import { combineReducer } from 'redux'
+
+const rootReducer = combineReducer({
+  todo: () => ({
+    description: 'Ler livro',
+    list: [
+      {
+        _id: 1,
+        description: 'Pagar fatura cartão',
+        done: true
+      },
+      {
+        _id: 2,
+        description: 'Reunião com equipe as 10:00',
+        done: false
+      },
+      {
+        _id: 3,
+        description: 'Ir ao dentista as 09:00',
+        done: false
+      },
+    ]
+  })
+})
+
+export default rootReducer
